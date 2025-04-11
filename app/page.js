2054c71c12e9a5b0dc5896766ce6dffe702a0438
ezhar karim <ezharkarim19@gmail.com>
2025-04-11 12:19:41 +0500

    Complete the hero Section

--- app/page.js
+++ app/page.js
@@ -2,8 +2,11 @@ import HeroSection from "@/src/app/components/HeroSection";
 
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen bg-[#121212] container mx-auto px-12 py-4">
-      <HeroSection />
+    <main className="flex flex-col min-h-screen bg-[#121212]">
+     <div className=" container mx-auto px-12 py-4">
+     <HeroSection />
+     </div>
+      
     </main>
   );
 }
