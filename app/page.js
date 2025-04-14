ea04f4bd741bfe90bab9d81c94d33e03ea711841
Ezhar karim <ezharkarim19@gmail.com>
2025-04-15 01:47:36 +0500

    Complete Navbar (#1)
    * Complete the hero Section
    
    * Complete Navbar

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
 import HeroSection from "@/src/app/components/HeroSection";
+import Navbar from "@/src/app/components/Navbar";
 
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen bg-[#121212] container mx-auto px-12 py-4">
-      <HeroSection />
+    <main className="flex flex-col min-h-screen bg-[#121212]">
+      <Navbar/>
+     <div className=" container mt-24 mx-auto px-12 py-4">
+     <HeroSection />
+     </div>
+      
     </main>
   );
 }
