fd5a830990761f81b0c648c1d89105c2ce13331c
ezhar karim <ezharkarim19@gmail.com>
2025-04-17 00:54:43 +0500

    complete about us page

--- src/app/components/TabButtons.jsx
+++ src/app/components/TabButtons.jsx
@@ -7,7 +7,7 @@ const variants = {
 };
 
 const TabButtons = ({ active, selectTab, children }) => {
-  const buttonClasses = active ? "text-white border-b border-purple-500" : "text-[#ADB7BE]";
+  const buttonClasses = active ? "text-white border-b2  border-purple-500" : "text-[#ADB7BE]";
 
   return (
     <button onClick={selectTab}>
