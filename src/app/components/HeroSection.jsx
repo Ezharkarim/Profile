69e922eca9601133667d357a231d388f844517ad
Ezhar karim <ezharkarim19@gmail.com>
2025-04-21 15:54:41 +0500

    our project section completed (#3)

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -50,7 +50,7 @@ const HeroSection = () => {
         <div className=" col-span-5 place-self-center mt-4 lg:mt-0 ">
           <div className=" bg-[#181818] rounded-full w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative overflow-hidden">
             <Image
-              src="/ezu.png"
+              src="/images/ezu.png"
               alt="image here"
               className=" absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
