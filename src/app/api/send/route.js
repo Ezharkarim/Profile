584eb21ebfa37398368a5813fe55e7d6f524ae63
ezhar karim <ezharkarim19@gmail.com>
2025-05-03 23:55:32 +0500

    fix the bugs on contact form

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,30 +1,28 @@
-// src/app/api/send/route.js
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST(req) {
+export async function POST(req, res) {
+  const { email, subject, message } = await req.json();
+  console.log(email, subject, message);
   try {
-    const { email, subject, message } = await req.json();
-
     const data = await resend.emails.send({
       from: fromEmail,
-      to: [fromEmail],
+      to: [fromEmail, email],
       subject: subject,
       react: (
         <>
           <h1>{subject}</h1>
-          <p>New message from: {email}</p>
+          <p>Thank you for contacting us!</p>
+          <p>New message submitted:</p>
           <p>{message}</p>
         </>
       ),
     });
-
-    return NextResponse.json({ success: true, data });
+    return NextResponse.json(data);
   } catch (error) {
-    console.error("Email send error:", error);
-    return NextResponse.json({ success: false, error: error.message }, { status: 500 });
+    return NextResponse.json({ error });
   }
 }
